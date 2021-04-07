de666fa62654a011adb0b57f7d1d029a7cbe2b3f
Imon <contact@imonweb.co.uk>
2021-04-07 03:57:52 +0100

    materialize-css

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,27 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader','css-loader']
-      } 
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: 'file-loader?name=images/[name].[ext]',
+      },
+      // font-awesome
+      {
+        test: /font-awesome\.config\.js/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'font-awesome-loader' }
+        ]
+      }, 
     ]
   }
   
