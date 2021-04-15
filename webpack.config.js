2641766d200be67e357f6fdfb11829e8a9a95f77
Imon <contact@imonweb.co.uk>
2021-04-15 23:32:00 +0100

    ajax

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
  
 
 module.exports = {
-  mode: 'devlopment',
+  mode: 'development',
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
