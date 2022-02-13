034f1882c68c89c89d6f51ac9d4635ca6d453a13
imonweb <contact@imonweb.co.uk>
2022-02-13 22:04:30 +0000

    post request

--- AJAX/Callbacks/easyhttp/app.js
+++ AJAX/Callbacks/easyhttp/app.js
@@ -1,6 +1,44 @@
 const http = new easyHTTP;
 
 // Get Posts
-const posts = http.get('https://jsonplaceholder.typicode.com/posts');
+// const posts = http.get('https://jsonplaceholder.typicode.com/posts');
+// http.get('https://jsonplaceholder.typicode.com/posts', 
+// function(error, posts) {
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+  
+// });
 
-console.log(posts);
+
+// console.log(posts);
+
+// Get single post
+/*
+http.get('https://jsonplaceholder.typicode.com/posts/1', 
+function(error, post) {
+  if(err){
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+  
+});
+*/
+
+/*  Create data */
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+   if(err){
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
--- AJAX/Callbacks/easyhttp/easyhttp.js
+++ AJAX/Callbacks/easyhttp/easyhttp.js
@@ -7,13 +7,28 @@ easyHTTP.prototype.get = function(url, callback){
   this.http.open('GET', url, true);
 
   let that = this;
-  that.http.onload = function() {
+  this.http.onload = function() {
     if(that.http.status === 200) {
       // return that.http.responseText;
-      callback(that.http.responseText);
+      callback(null, that.http.responseText);
+    } else {
+      callback('Error: ' + that.http.status);
     }
   }
 
   this.http.send();
 }
+
 //Make HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback){
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let that = this;
+  this.http.onload = function() {
+    callback(null, that.http.responseText);
+  }
+  this.http.send(JSON.stringify(data));
+}
+
+// Makee an HTTP PUT Request
