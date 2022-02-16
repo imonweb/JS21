bdbde1d3b527d1208108032977d7738044c9993b
imonweb <contact@imonweb.co.uk>
2022-02-16 02:00:32 +0000

    github-finder initial setup

---
+++ Arrow_Functions/app.js
@@ -0,0 +1,14 @@
+// const sayHello = function() {
+//   console.log('Hello');
+// }
+
+// const sayHello = ()=> {
+//   console.log('Hello');
+// }
+
+// One line function does not need braces
+// const sayHello = () => console.log('Hello');
+
+const sayHello = () => 'Hello';
+
+console.log(sayHello());
