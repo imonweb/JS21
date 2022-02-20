c8c744ece7b60446d314573f2803e91fe2c641c7
imonweb <contact@imonweb.co.uk>
2022-02-20 00:24:34 +0000

    github finder completed

--- github-finder/github.js
+++ github-finder/github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'a1886a9c0d7842f1367c';
     this.client_secret = 'f76a7d5cd370454683fc941de47b2eb2e48df0fc';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
--- github-finder/ui.js
+++ github-finder/ui.js
@@ -34,8 +34,35 @@ class UI {
     `;
   }
 
+  // Show user repos
+  showRepos(repos){
+    let output = '';
+
+    repos.forEach(function(repo){
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_repo}" target="_blank">${repo.name}
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}></span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}></span>
+              <span class="badge badge-success">Forks: ${repo.forms_count}></span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    // output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
   // show alert message
   showAlert(message, className){
+    // Clear any remaining alerts
+    this.clearAlert();
     // Create div
     const div = document.createElement('div');
     // Add classes
@@ -48,6 +75,20 @@ class UI {
     const search = document.querySelector('.search');
     // Insert alert
     container.insertBefore(div, search);
+
+    // Timeout after 3 secs
+    setTimeout( () => {
+      this.clearAlert()
+    }, 3000); 
+  }
+
+  // Clear alert message
+  clearAlert(){
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert){
+      currentAlert.remove();
+    }
   }
 
   // clear profile
